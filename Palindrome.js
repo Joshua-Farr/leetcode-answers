936fca987f7b6e2fd70208b3e522abd0e576bc6c
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-06-14 22:39:34 -0500

    Fixed Palindrome

--- Palindrome.js
+++ Palindrome.js
@@ -1,13 +1,14 @@
 var isPalindrome = function (x) {
   numberString = x.toString();
+  numberStringLength = numberString.length;
 
-  for (let i = 0; i < numberString.length; i++) {
-    if (numberString[i] !== numberString[numberString.length - i]) {
+  for (let i = 0; i < numberStringLength - 1; i++) {
+    if (numberString[i] !== numberString[numberStringLength - i - 1]) {
       return false;
     }
   }
   return true;
 };
 
-const result = isPalindrome(122);
+const result = isPalindrome(121);
 console.log("Result: ", result);
