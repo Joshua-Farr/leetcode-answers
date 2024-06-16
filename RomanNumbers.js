bce77913aba8b92765a45e2ac7c47cc948d39bcf
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-06-16 16:01:54 -0500

    Working code

--- RomanNumbers.js
+++ RomanNumbers.js
@@ -15,9 +15,31 @@ var romanToInt = function (s) {
 
   let numeralSum = 0;
 
-  //Need to work backwards from the end of the roman numberal
+  //Need to work backwards from the end of the roman numeral
 
-  for (let i = s.length - 1; i >= 0; i--) {}
+  for (let i = s.length - 1; i >= 0; i--) {
+    const value = numberMap.get(s.charAt(i));
 
+    const test1 =
+      s.charAt(i) === "V" || s.charAt(i) === "L" || s.charAt(i) === "D";
+
+    const test2 =
+      s.charAt(i - 1) === "I" ||
+      s.charAt(i - 1) === "X" ||
+      s.charAt(i - 1) === "C";
+
+    if (test1 && test2) {
+      const reducerValue = 2 * numberMap.get(s.charAt(i - 1));
+      numeralSum += value - reducerValue;
+
+      console.log("We found a char!, ", s.charAt(i), reducerValue);
+    } else {
+      numeralSum += value;
+    }
+  }
   return numeralSum;
 };
+
+const interger = romanToInt("XV");
+
+console.log(interger);
